26a97b1c6df1f981c011333a48e9afea51b4134f
sakami0000 <sa-ka-mi-0000@outlook.jp>
2019-01-10 07:01:57 +0900

    config

---
+++ config.py
@@ -0,0 +1,24 @@
+import os
+import random
+
+import numpy as np
+import torch
+
+embed_size = 300
+max_features = 95000
+maxlen = 72
+
+batch_size = 1536
+train_epochs = 5
+n_splits = 5
+
+seed = 1029
+
+
+def seed_torch(seed=1029):
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
