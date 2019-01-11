3d43445a8aa678f200f5b9afd66a06618119f034
sakami0000 <sa-ka-mi-0000@outlook.jp>
2019-01-11 13:59:03 +0900

    preprocessing

---
+++ preprocessing.py
@@ -0,0 +1,83 @@
+import re
+
+puncts = [',', '.', '"', ':', ')', '(', '-', '!', '?', '|', ';', "'", '$', '&',
+          '/', '[', ']', '>', '%', '=', '#', '*', '+', '\\', '•',  '~', '@', '£',
+          '·', '_', '{', '}', '©', '^', '®', '`',  '<', '→', '°', '€', '™', '›',
+          '♥', '←', '×', '§', '″', '′', 'Â', '█', '½', 'à', '…', '“', '★', '”',
+          '–', '●', 'â', '►', '−', '¢', '²', '¬', '░', '¶', '↑', '±', '¿', '▾',
+          '═', '¦', '║', '―', '¥', '▓', '—', '‹', '─', '▒', '：', '¼', '⊕',
+          '▼', '▪', '†', '■', '’', '▀', '¨', '▄', '♫', '☆', 'é', '¯', '♦',
+          '¤', '▲', 'è', '¸', '¾', 'Ã', '⋅', '‘', '∞', '∙', '）', '↓', '、', '│',
+          '（', '»', '，', '♪', '╩', '╚', '³', '・', '╦', '╣', '╔', '╗', '▬',
+          '❤', 'ï', 'Ø', '¹', '≤', '‡', '√']
+
+misspell_dict = {"aren't": "are not", "can't": "cannot", "couldn't": "could not",
+                 "didn't": "did not", "doesn't": "does not", "don't": "do not",
+                 "hadn't": "had not", "hasn't": "has not", "haven't": "have not",
+                 "he'd": "he would", "he'll": "he will", "he's": "he is",
+                 "i'd": "I had", "i'll": "I will", "i'm": "I am", "isn't": "is not",
+                 "it's": "it is", "it'll": "it will", "i've": "I have", "let's": "let us",
+                 "mightn't": "might not", "mustn't": "must not", "shan't": "shall not",
+                 "she'd": "she would", "she'll": "she will", "she's": "she is",
+                 "shouldn't": "should not", "that's": "that is", "there's": "there is",
+                 "they'd": "they would", "they'll": "they will", "they're": "they are",
+                 "they've": "they have", "we'd": "we would", "we're": "we are",
+                 "weren't": "were not", "we've": "we have", "what'll": "what will",
+                 "what're": "what are", "what's": "what is", "what've": "what have",
+                 "where's": "where is", "who'd": "who would", "who'll": "who will",
+                 "who're": "who are", "who's": "who is", "who've": "who have",
+                 "won't": "will not", "wouldn't": "would not", "you'd": "you would",
+                 "you'll": "you will", "you're": "you are", "you've": "you have",
+                 "'re": " are", "wasn't": "was not", "we'll": " will", "tryin'": "trying"}
+
+
+def clean_text(x):
+    x = str(x)
+    for punct in puncts:
+        if punct in x:
+            x = x.replace(punct, f' {punct} ')
+    return x
+
+
+def clean_numbers(x):
+    x = re.sub('[0-9]{5,}', '#####', x)
+    x = re.sub('[0-9]{4}', '####', x)
+    x = re.sub('[0-9]{3}', '###', x)
+    x = re.sub('[0-9]{2}', '##', x)
+    return x
+
+
+def _get_misspell(misspell_dict):
+    misspell_re = re.compile('(%s)' % '|'.join(misspell_dict.keys()))
+    return misspell_dict, misspell_re
+
+
+misspellings, misspellings_re = _get_misspell(misspell_dict)
+
+
+def replace_typical_misspell(text):
+
+    def replace(match):
+        return misspellings[match.group(0)]
+
+    return misspellings_re.sub(replace, text)
+
+
+def add_features(embeddings_index, df):
+
+    df['question_text'] = df['question_text'].apply(lambda x: str(x))
+    df['total_length'] = df['question_text'].apply(len)
+    df['capitals'] = df['question_text'].apply(lambda comment: sum(1 for c in comment if c.isupper()))
+    df['caps_vs_length'] = df.apply(
+        lambda row: float(row['capitals']) / float(row['total_length']), axis=1)
+
+    df['num_words'] = df.question_text.str.count('\S+')
+    df['num_unique_words'] = df['question_text'].apply(
+        lambda comment: len(set(w for w in comment.split())))
+    df['words_vs_unique'] = df['num_unique_words'] / df['num_words']
+
+    df['oov'] = df['question_text'].apply(
+        lambda comment: sum(1 for w in comment.split() if embeddings_index.get(w) is None))
+    df['oov_vs_words'] = df['oov'] / df['num_words']
+
+    return df
