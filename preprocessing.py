8c04a488b5307a49b3aed04f0d98a3f2c9ba5707
sakami0000 <sa-ka-mi-0000@outlook.jp>
2019-01-13 15:17:41 +0900

    nbfeaturer

--- preprocessing.py
+++ preprocessing.py
@@ -1,4 +1,9 @@
 import re
+import string
+
+import numpy as np
+from scipy import sparse
+from sklearn.base import BaseEstimator, TransformerMixin
 
 puncts = [',', '.', '"', ':', ')', '(', '-', '!', '?', '|', ';', "'", '$', '&',
           '/', '[', ']', '>', '%', '=', '#', '*', '+', '\\', '•',  '~', '@', '£',
@@ -81,3 +86,28 @@ def add_features(embeddings_index, df):
     df['oov_vs_words'] = df['oov'] / df['num_words']
 
     return df
+
+
+class NBFeaturer(BaseEstimator, TransformerMixin):
+    def __init__(self, alpha=1):
+        self.alpha = alpha
+
+    def pr(self, x, y_i, y):
+        p = x[y == y_i].sum(0)
+        return (p + self.alpha) / ((y == y_i).sum() + self.alpha)
+
+    def fit(self, x, y):
+        self._r = sparse.csr_matrix(np.log(self.pr(x, 1, y) / self.pr(x, 0, y)))
+        return self
+
+    def preprocess_x(self, x, r):
+        return x.multiply(r)
+
+    def transform(self, x):
+        x_nb = self.preprocess_x(x, self._r)
+        return x_nb
+
+
+def tokenize(s):
+    re_tok = re.compile(f'([{string.punctuation}“”¨«»®´·º½¾¿¡§£₤‘’])')
+    return re_tok.sub(r' \1 ', s).split()
