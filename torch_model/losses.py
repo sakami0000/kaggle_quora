5fa6fb3c6845e3a44efad74ecf0c1f7b29cb678a
sakami0000 <sa-ka-mi-0000@outlook.jp>
2019-01-21 02:49:04 +0900

    focalloss

--- torch_model/losses.py
+++ torch_model/losses.py
@@ -14,3 +14,26 @@ class LogCosh(nn.Module):
             return x + F.softplus(-2. * x) - np.log(2.)
 
         return torch.mean(_logcosh(target - preds))
+
+
+class FocalLoss(nn.Module):
+    def __init__(self, alpha=1, gamma=2, logits=False, reduce=True):
+        super(FocalLoss, self).__init__()
+        self.alpha = alpha
+        self.gamma = gamma
+        self.logits = logits
+        self.reduce = reduce
+
+    def forward(self, inputs, targets):
+        if self.logits:
+            bce_loss = F.binary_cross_entropy_with_logits(inputs, targets, reduce=False)
+        else:
+            bce_loss = F.binary_cross_entropy(inputs, targets, reduce=False)
+
+        pt = torch.exp(-bce_loss)
+        f_loss = self.alpha * (1 - pt) ** self.gamma * bce_loss
+
+        if self.reduce:
+            return torch.mean(f_loss)
+        else:
+            return f_loss
