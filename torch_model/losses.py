6e594b61d17ae30dea3f6f429b6ab593f04955ef
sakami0000 <sa-ka-mi-0000@outlook.jp>
2019-01-23 01:33:46 +0900

    lovasz loss

--- torch_model/losses.py
+++ torch_model/losses.py
@@ -37,3 +37,46 @@ class FocalLoss(nn.Module):
             return torch.mean(f_loss)
         else:
             return f_loss
+
+
+class LovaszLoss(nn.Module):
+    def __init__(self, activation='relu'):
+        assert activation in ['relu', 'elu1p'], f'Invalid activation: {activation}'
+        super(LovaszLoss, self).__init__()
+
+        if activation == 'relu':
+            self.activation = F.relu
+        else:
+            self.activation = self.elu1p
+
+    @staticmethod
+    def elu1p(inputs):
+        return F.elu(inputs + 1)
+
+    @staticmethod
+    def lovasz_grad(gt_sorted):
+        p = len(gt_sorted)
+        gts = gt_sorted.sum()
+
+        intersection = gts - gt_sorted.float().cumsum(0)
+        union = gts + (1 - gt_sorted).float().cumsum(0)
+
+        jaccard = 1. - intersection / union
+        if p > 1:
+            jaccard[1:p] = jaccard[1:p] - jaccard[0:-1]
+
+        return jaccard
+
+    def forward(self, logits, labels):
+        if len(labels) == 0:
+            # only void pixels, the gradients should be 0
+            return logits.sum() * 0.
+        signs = 2. * labels.float() - 1.
+        errors = (1. - logits * signs)
+        errors_sorted, perm = torch.sort(errors, dim=0, descending=True)
+        perm = perm.data
+        gt_sorted = labels[perm]
+        grad = self.lovasz_grad(gt_sorted)
+        loss = torch.dot(torch.squeeze(self.activation(errors_sorted)),
+                         torch.squeeze(grad))
+        return loss
