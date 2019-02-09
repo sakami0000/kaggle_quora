96c514274b89c5d8137754d0604bd0b8d8a260e7
sakami0000 <sa-ka-mi-0000@outlook.jp>
2019-02-10 02:18:27 +0900

    SentenceLengthDataset

--- torch_model/utils.py
+++ torch_model/utils.py
@@ -3,6 +3,7 @@ import random
 
 import numpy as np
 import torch
+from torch.utils.data import Dataset
 
 
 def seed_torch(seed=1029):
@@ -12,3 +13,19 @@ def seed_torch(seed=1029):
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
     torch.backends.cudnn.deterministic = True
+
+
+class SentenceLengthDataset(Dataset):
+    def __init__(self, dataset, data_len, data_features):
+        self.dataset = dataset
+        self.data_len = data_len
+        self.data_features = data_features
+
+    def __getitem__(self, index):
+        data = self.dataset[index]
+        data_len = self.data_len[index]
+        data_features = self.data_features[index]
+        return data, data_len, data_features
+
+    def __len__(self):
+        return len(self.dataset)
